c23c7fb6567261c9ea43b8dbd6fd5576906773d5
yashw__72 <yashwarungase5@gmail.com>
2026-02-17 23:49:53 +0530

    Add Sqaure of a Sorted Array(Brute Force) Approch

---
+++ Array/977. Squares of a Sorted Array(Brute Force).cpp
@@ -0,0 +1,28 @@
+// 977. Squares of a Sorted Array
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) 
+    {
+    int n=nums.size();
+    vector<int> ans(n);
+
+    for(int i=0;i<n;i++)
+    {
+        ans[i]=nums[i]*nums[i];
+    }
+    sort(ans.begin(),ans.end());
+    return ans;
+    }
+    return ans;    
+    }
+};
