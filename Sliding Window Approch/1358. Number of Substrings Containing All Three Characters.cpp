95a7bb87360a68d994501611e0e402b931288aab
yashw__72 <yashwarungase5@gmail.com>
2026-08-14 00:34:36 +0530

    Add 1358

---
+++ Sliding Window Approch/1358. Number of Substrings Containing All Three Characters.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) 
+    {
+        int count=0;
+        int lastseen[3]={-1,-1,-1};
+
+        for(int i=0;i<s.length();i++)
+        {
+            lastseen[s[i]-'a']=i;
+
+            if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1)
+            {
+                count=count+1+min({lastseen[0],lastseen[1],lastseen[2]});
+            }
+        }
+    return count;
+    }
+};
