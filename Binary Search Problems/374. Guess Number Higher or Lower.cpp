5326ab8507ee1d83ff4824676f15b4885c5f14c7
yashw__72 <yashwarungase5@gmail.com>
2026-08-18 23:27:02 +0530

    Add 374 to Binary Search Folder

---
+++ Binary Search Problems/374. Guess Number Higher or Lower.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) 
+    { 
+        int low=1;
+        int high=n;
+
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int ans=guess(mid);
+
+            if(ans==0)
+            {
+                return mid;
+            }
+            else if(ans==1)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+    return -1;
+    }
+
+};
